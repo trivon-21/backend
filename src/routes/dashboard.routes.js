15806eaa7d97e83c7fcc6182107a848b004b21a5
Sathnuwan Dassana <95568551+sDassana@users.noreply.github.com>
2026-08-14 19:05:47 +0530

    feat: add Postman collection and test documentation for inventory API endpoints

--- src/routes/dashboard.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { protect } = require("../middleware/protect");
-const { getDashboard } = require("../controllers/dashboard.controller");
-
-router.use(protect);
-
-router.get("/", getDashboard);
-
-module.exports = router;
